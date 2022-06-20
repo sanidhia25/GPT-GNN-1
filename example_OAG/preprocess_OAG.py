de5c18f850c8ec6dbca77065670ad2bc554a2a62
sanidhia25 <67638569+sanidhia25@users.noreply.github.com>
2022-06-20 13:17:10 +0530

    my PC updates

--- example_OAG/preprocess_OAG.py
+++ example_OAG/preprocess_OAG.py
@@ -1,9 +1,16 @@
 from transformers import *
 
-from data import *
+from collections import defaultdict
+import torch
+import data
 import gensim
 from gensim.models import Word2Vec
 from tqdm import tqdm
+import scipy.sparse as sp
+import numpy as np
+import dill
+import pandas as pd
+import utils
 # from tqdm import tqdm_notebook as tqdm   # Comment this line if using jupyter notebook
 
 
@@ -18,7 +25,7 @@ parser.add_argument('--input_dir', type=str, default='./data/oag_raw',
                     help='The address to store the original data directory.')
 parser.add_argument('--output_dir', type=str, default='./data/oag_output',
                     help='The address to output the preprocessed graph.')
-parser.add_argument('--cuda', type=int, default=0,
+parser.add_argument('--cuda', type=int, default=-1,
                     help='Avaiable GPU ID')
 parser.add_argument('--domain', type=str, default='_CS',
                     help='CS, Medical or All: _CS or _Med or (empty)')
@@ -31,24 +38,26 @@ args = parser.parse_args()
 test_time_bar = 2016
 
 cite_dict = defaultdict(lambda: 0)
-with open(args.input_dir + '/PR%s_20190919.tsv' % args.domain) as fin:
+print("Started reading PR file 1.........")
+with open(args.input_dir + '/PR%s_20190919.tsv' % args.domain,encoding="utf-8") as fin:
     fin.readline()
-    for l in tqdm(fin, total = sum(1 for line in open(args.input_dir + '/PR%s_20190919.tsv' % args.domain))):
+    for l in tqdm(fin, total = sum(1 for line in open(args.input_dir + '/PR%s_20190919.tsv' % args.domain,encoding="utf-8"))):
         l = l[:-1].split('\t')
         cite_dict[l[1]] += 1
-        
-        
+print("Completed reading PR file 1")
+
 pfl = defaultdict(lambda: {})
-with open(args.input_dir + '/Papers%s_20190919.tsv' % args.domain) as fin:
+print("Started readinf Papers file 2......")
+with open(args.input_dir + '/Papers%s_20190919.tsv' % args.domain,encoding="utf-8") as fin:
     fin.readline()
-    for l in tqdm(fin, total = sum(1 for line in open(args.input_dir + '/Papers%s_20190919.tsv' % args.domain))):
+    for l in tqdm(fin, total = sum(1 for line in open(args.input_dir + '/Papers%s_20190919.tsv' % args.domain,encoding="utf-8"))):
         l = l[:-1].split('\t')
         bound = min(2020 - int(l[1]), 20) * args.citation_bar
         if cite_dict[l[0]] < bound or l[0] == '' or l[1] == '' or l[2] == '' or l[3] == '' and l[4] == '' or int(l[1]) < 1900:
             continue
         pi = {'id': l[0], 'title': l[2], 'type': 'paper', 'time': int(l[1])}
         pfl[l[0]] = pi
-        
+print("Completed reading Papers file 2")
       
 if args.cuda != -1:
     device = torch.device("cuda:" + str(args.cuda))
@@ -60,10 +69,11 @@ model = XLNetModel.from_pretrained('xlnet-base-cased',
                                     output_hidden_states=True,
                                     output_attentions=True).to(device)
         
-               
-with open(args.input_dir + '/PAb%s_20190919.tsv' % args.domain) as fin:
+
+print("Started reading PAb file 3")   
+with open(args.input_dir + '/PAb%s_20190919.tsv' % args.domain, encoding='utf-8') as fin:
     fin.readline()
-    for l in tqdm(fin, total = sum(1 for line in open(args.input_dir + '/PAb%s_20190919.tsv' % args.domain, 'r'))):
+    for l in tqdm(fin, total = sum(1 for line in open(args.input_dir + '/PAb%s_20190919.tsv' % args.domain, 'r',encoding='utf-8'))):
         try:
             l = l.split('\t')
             if l[0] in pfl:
@@ -75,57 +85,70 @@ with open(args.input_dir + '/PAb%s_20190919.tsv' % args.domain) as fin:
                 pfl[l[0]]['emb'] = rep.tolist()
         except Exception as e:
             print(e)
-        
-        
+print("Completed readinf PAb file 3")       
+print("Dumping pfl....")
+af1 = open("vfl_pick.pk","wb")
+dill.dump(pfl,af1)
+af1.close()
+
         
 vfi_ids = {}
-with open(args.input_dir + '/vfi_vector.tsv') as fin:
-    for l in tqdm(fin, total = sum(1 for line in open(args.input_dir + '/vfi_vector.tsv'))):
+print("Started reading vfi file 4........")
+with open(args.input_dir + '/vfi_vector.tsv',encoding="utf-8") as fin:
+    for l in tqdm(fin, total = sum(1 for line in open(args.input_dir + '/vfi_vector.tsv',encoding="utf-8"))):
         l = l[:-1].split('\t')
         vfi_ids[l[0]] = True        
-        
-        
-graph = Graph()
+print("Completed reading vfi file 4")
+af2 = open("vfi_ids_pick.pk","wb")
+dill.dump(vfi_ids,af2)
+af2.close()
+
+graph = data.Graph()
 rem = []
-with open(args.input_dir + '/Papers%s_20190919.tsv' % args.domain) as fin:
+
+print("Started reading Papers file 5.........")
+with open(args.input_dir + '/Papers%s_20190919.tsv' % args.domain,encoding="utf-8") as fin:
     fin.readline()
-    for l in tqdm(fin, total = sum(1 for line in open(args.input_dir + '/Papers%s_20190919.tsv' % args.domain, 'r'))):
+    for l in tqdm(fin, total = sum(1 for line in open(args.input_dir + '/Papers%s_20190919.tsv' % args.domain, 'r',encoding="utf-8"))):
         l = l[:-1].split('\t')
         if l[0] not in pfl or l[4] != 'en' or 'emb' not in pfl[l[0]] or l[3] not in vfi_ids:
             continue
         rem += [l[0]]
         vi = {'id': l[3], 'type': 'venue', 'attr': l[-2]}
         graph.add_edge(pfl[l[0]], vi, time = int(l[1]), relation_type = 'PV_' + l[-2])
+print("Completed reading Papers file 5")
 pfl = {i: pfl[i] for i in rem}
 print(len(pfl))
 
-        
-with open(args.input_dir + '/PR%s_20190919.tsv' % args.domain) as fin:
+
+print("Started reading PR file 6..........")
+with open(args.input_dir + '/PR%s_20190919.tsv' % args.domain,encoding="utf-8") as fin:
     fin.readline()
-    for l in tqdm(fin, total = sum(1 for line in open(args.input_dir + '/PR%s_20190919.tsv' % args.domain))):
+    for l in tqdm(fin, total = sum(1 for line in open(args.input_dir + '/PR%s_20190919.tsv' % args.domain,encoding="utf-8"))):
         l = l[:-1].split('\t')
         if l[0] in pfl and l[1] in pfl:
             p1 = pfl[l[0]]
             p2 = pfl[l[1]]
             if p1['time'] >= p2['time']:
                 graph.add_edge(p1, p2, time = p1['time'], relation_type = 'PP_cite')
-        
+print("COmpleted reading PR file 6")
         
         
 ffl = {}
-with open(args.input_dir + '/PF%s_20190919.tsv' % args.domain) as fin:
+print("Started reading PF file 7.......")
+with open(args.input_dir + '/PF%s_20190919.tsv' % args.domain,encoding="utf-8") as fin:
     fin.readline()
-    for l in tqdm(fin, total = sum(1 for line in open(args.input_dir + '/PF%s_20190919.tsv' % args.domain))):
+    for l in tqdm(fin, total = sum(1 for line in open(args.input_dir + '/PF%s_20190919.tsv' % args.domain,encoding="utf-8"))):
         l = l[:-1].split('\t')
         if l[0] in pfl and l[1] in vfi_ids:
             ffl[l[1]] = True        
+print("Completed reading PF file 7")        
         
         
-        
-        
-with open(args.input_dir + '/FHierarchy_20190919.tsv') as fin:
+print("Started reading FHierarchy file 8.........")        
+with open(args.input_dir + '/FHierarchy_20190919.tsv',encoding="utf-8") as fin:
     fin.readline()
-    for l in tqdm(fin, total = sum(1 for line in open(args.input_dir + '/FHierarchy_20190919.tsv'))):
+    for l in tqdm(fin, total = sum(1 for line in open(args.input_dir + '/FHierarchy_20190919.tsv',encoding="utf-8"))):
         l = l[:-1].split('\t')
         if l[0] in ffl and l[1] in ffl:
             fi = {'id': l[0], 'type': 'field', 'attr': l[2]}
@@ -133,26 +156,27 @@ with open(args.input_dir + '/FHierarchy_20190919.tsv') as fin:
             graph.add_edge(fi, fj, relation_type = 'FF_in')
             ffl[l[0]] = fi
             ffl[l[1]] = fj        
+print("Completed reading FHierarchy file 8")        
         
         
-        
-        
-with open(args.input_dir + '/PF%s_20190919.tsv' % args.domain) as fin:
+print("Started reading PF file 9.........")       
+with open(args.input_dir + '/PF%s_20190919.tsv' % args.domain,encoding="utf-8") as fin:
     fin.readline()
-    for l in tqdm(fin, total = sum(1 for line in open(args.input_dir + '/PF%s_20190919.tsv' % args.domain))):
+    for l in tqdm(fin, total = sum(1 for line in open(args.input_dir + '/PF%s_20190919.tsv' % args.domain,encoding="utf-8"))):
         l = l[:-1].split('\t')
         if l[0] in pfl and l[1] in ffl and type(ffl[l[1]]) == dict:
             pi = pfl[l[0]]
             fi = ffl[l[1]]
             graph.add_edge(pi, fi, time = pi['time'], relation_type = 'PF_in_' + fi['attr'])        
-        
+print("Completed reading PF file 9")        
         
         
         
 coa = defaultdict(lambda: {})
-with open(args.input_dir + '/PAuAf%s_20190919.tsv' % args.domain) as fin:
+print("Started reading PAuAf file 10........")
+with open(args.input_dir + '/PAuAf%s_20190919.tsv' % args.domain,encoding="utf-8") as fin:
     fin.readline()
-    for l in tqdm(fin, total = sum(1 for line in open(args.input_dir + '/PAuAf%s_20190919.tsv' % args.domain))):
+    for l in tqdm(fin, total = sum(1 for line in open(args.input_dir + '/PAuAf%s_20190919.tsv' % args.domain,encoding="utf-8"))):
         l = l[:-1].split('\t')
         if l[0] in pfl and l[2] in vfi_ids:
             pi = pfl[l[0]]
@@ -160,6 +184,7 @@ with open(args.input_dir + '/PAuAf%s_20190919.tsv' % args.domain) as fin:
             fi = {'id': l[2], 'type': 'affiliation'}
             coa[l[0]][int(l[-1])] = ai
             graph.add_edge(ai, fi, relation_type = 'in')
+print("Completed reading PauAf file 10.")
 
 for pid in tqdm(coa):
     pi = pfl[pid]
@@ -175,20 +200,19 @@ for pid in tqdm(coa):
         
         
         
-        
-with open(args.input_dir + '/vfi_vector.tsv') as fin:
-    for l in tqdm(fin, total = sum(1 for line in open(args.input_dir + '/vfi_vector.tsv'))):
+print("Started reading vfi file 11........")       
+with open(args.input_dir + '/vfi_vector.tsv',encoding="utf-8") as fin:
+    for l in tqdm(fin, total = sum(1 for line in open(args.input_dir + '/vfi_vector.tsv',encoding="utf-8"))):
         l = l[:-1].split('\t')
         ser = l[0]
         for idx in ['venue', 'field', 'affiliation']:
             if ser in graph.node_forward[idx]:
                 graph.node_bacward[idx][graph.node_forward[idx][ser]]['node_emb'] = np.array(l[1].split(' '))        
-        
-        
+print("Completed reading vfi file 11")
 
-        
-with open(args.input_dir + '/SeqName%s_20190919.tsv' % args.domain) as fin:
-    for l in tqdm(fin, total = sum(1 for line in open(args.input_dir + '/SeqName%s_20190919.tsv' % args.domain))):
+print("Started reading SeqName file 12.........")        
+with open(args.input_dir + '/SeqName%s_20190919.tsv' % args.domain,encoding="utf-8") as fin:
+    for l in tqdm(fin, total = sum(1 for line in open(args.input_dir + '/SeqName%s_20190919.tsv' % args.domain,encoding="utf-8"))):
         l = l[:-1].split('\t')
         key = l[2]
         if key in ['conference', 'journal', 'repository', 'patent']:
@@ -198,7 +222,8 @@ with open(args.input_dir + '/SeqName%s_20190919.tsv' % args.domain) as fin:
         if l[0] in graph.node_forward[key]:
             s = graph.node_bacward[key][graph.node_forward[key][l[0]]]
             s['name'] = l[1]     
-    
+print("Completed reading Seqname file 12")
+
 '''
     Calculate the total citation information as node attributes.
 '''
@@ -253,7 +278,7 @@ for _type in graph.node_bacward:
             continue
         i = np.array(i).T
         v = np.ones(i.shape[1])
-        m = normalize(sp.coo_matrix((v, i), \
+        m = utils.normalize(sp.coo_matrix((v, i), \
             shape=(len(graph.node_bacward[_type]), len(graph.node_bacward['paper']))))
         out = m.dot(cv)
         d['emb'] = list(out)
@@ -270,7 +295,7 @@ for _rel in graph.edge_list['affiliation']['author']:
             i += [[j, t]]
 i = np.array(i).T
 v = np.ones(i.shape[1])
-m = normalize(sp.coo_matrix((v, i), \
+m = utils.normalize(sp.coo_matrix((v, i), \
     shape=(len(graph.node_bacward['affiliation']), len(graph.node_bacward['author']))))
 out = m.dot(cv)
 d['emb'] = list(out)
